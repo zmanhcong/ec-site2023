5e3561967fe2ddab9fc13d2bd1e183b249ab5285
nguyenmanhcong <cng.buff@gmail.com>
2023-03-09 23:50:21 +0900

    create project

---
+++ src/main/java/cng/ecsite/CngApplication.java
@@ -0,0 +1,13 @@
+package cng.ecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CngApplication.class, args);
+	}
+
+}
